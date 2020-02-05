28af8e7db2f2ebba4b8481e5ab093873951b75bb
CamiMeneses <CamiMeneses@github.com>
2020-02-04 23:22:59 -0500

    Add validations for events, movies, seats and user

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,4 +1,6 @@
 class Seat < ApplicationRecord
     belongs_to :event
     belongs_to :reservation, optional: true
+
+    validates_presence_of :name, :event_id, :price, :reservation_id, :status
 end
